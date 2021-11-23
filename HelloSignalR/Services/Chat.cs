12ed816d2a1c37bbe02e242057e52993863f3050
Ken Sim <kenspc@hotmail.com>
2021-11-23 11:10:28 +0800

    Add project files.

---
+++ HelloSignalR/Services/Chat.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloSignalR.Services
+{
+    public class Chat : Hub
+    {
+        public Task BroadcastMessage(string name, string message) =>
+            Clients.All.SendAsync("broadcastMessage", name, message);
+
+        public Task Echo(string name, string message) =>
+            Clients.Client(Context.ConnectionId)
+                   .SendAsync("echo", name, $"{message} (echo from server)");
+    }
+}
